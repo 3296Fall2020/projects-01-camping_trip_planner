6952aaf3a93adac00fd219f7d1741eaade7c88ce
Seb Tota <kidokyo1@gmail.com>
2020-11-02 16:11:38 -0500

    Added captcha check in wrapper. Started signup api route. Added missing requirements.

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, redirect
+from functools import wraps
 from flask_cors import CORS
 from src import auth
 
@@ -7,6 +8,21 @@ from src import auth
 
 app = Flask(__name__)
 CORS(app)
+
+
+def captcha_check(protected_function):
+    @wraps(protected_function)
+    def wrapper(*args, **kwargs):
+        data = request.get_json(force=True)  # Force body to be read as JSON
+        g_recaptcha_response = data.get('recap_response')
+
+        # Verify recaptcha is authenticated before checking password (prevent bot spamming)
+        if not auth.verify_recaptcha(g_recaptcha_response):
+            # Failed re-captcha verification
+            return jsonify({'status': 401, 'error': 'failed-recaptcha'})
+
+        return protected_function(*args, **kwargs)
+    return wrapper
 
 
 @app.route('/', methods=['GET'])
@@ -20,16 +36,11 @@ def home():
 
 
 @app.route('/login', methods=['POST'])
+@captcha_check
 def login():
     data = request.get_json(force=True)  # Force body to be read as JSON
     email = data.get('email')
     password = data.get('password')
-    g_recaptcha_response = data.get('recap_response')
-
-    # Verify recaptcha is authenticated before checking password (prevent bot spamming)
-    if not auth.verify_recaptcha(g_recaptcha_response):
-        # Failed re-captcha verification
-        return jsonify({'status': 401, 'error': 'failed-recaptcha'})
 
     # Authenticate users account and password
     if auth.verify_password(email, password):
@@ -41,7 +52,26 @@ def login():
 
 
 @app.route('/signup', methods=['POST'])
+@captcha_check
 def signup():
+    data = request.get_json(force=True)  # Force body to be read as JSON
+    first_name = data.get('first_name')
+    last_name = data.get('last_name')
+    email = data.get('email')
+    password = data.get('password')
+    g_recaptcha_response = data.get('recap_response')
+
+    # Verify recaptcha is authenticated before checking password (prevent bot spamming)
+    if not auth.verify_recaptcha(g_recaptcha_response):
+        # Failed re-captcha verification
+        return jsonify({'status': 401, 'error': 'failed-recaptcha'})
+
+    # TODO check if email already exists in the db
+    if 1 == 0:
+        return jsonify({'status': 400, 'error': 'user-already-exists'})
+
+
+
     return ""
 
 
