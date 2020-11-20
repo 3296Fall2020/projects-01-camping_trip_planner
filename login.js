730a559ec05c5c6f528e284a04202986601e3ec3
Seb Tota <kidokyo1@gmail.com>
2020-11-20 15:05:04 -0500

    Take user to profile page if already logged in.

--- login.js
+++ login.js
@@ -1,3 +1,7 @@
+if (sessionStorage.getItem('status') != null) {
+    window.location.href="profile.html";
+}
+
 const urlParams = new URLSearchParams(window.location.search);
 const signupCheck = urlParams.get('signup');
 
