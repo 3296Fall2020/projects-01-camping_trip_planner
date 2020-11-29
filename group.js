b069c084809b97eb6a17f43be36c9c80410de60f
Ryan O'Connor <tul49381@temple.edu>
2020-11-29 16:11:27 -0500

    Cleaned up code. Multi-table functionality.

--- group.js
+++ group.js
@@ -74,71 +74,91 @@ function createListItem(item) {
     return listItem;
 }
 
-$( document ).ready(function() {
+function getTableDivId(num){
+   switch(num) {
+        case num = 0:
+            return "#data-table1"
+        case num = 1:
+            return "#data-table2"
+        case num = 2:
+            return "#data-table3"
+        case num = 3:
+            return "#data-table4"
+        case num = 4:
+            return "#data-table5"
+    }
+}
 
+//render a single table
+function renderTable(tableDivId){
+    getElements().then(ret => {
+        console.log(ret);
+        console.log(ret['status']);
+        if (ret['status'] === 200) {
+            console.log("Creating list display");
+
+            const elements = ret['elements'];
+            console.log(elements);
+
+            let dataSet = [];
+            console.log(139);
+            for (let i = 0; i < elements.length; i++) {
+                dataSet.push([0, elements[i]['item-name'], elements[i]['item-quantity'], elements[i]['item-cost'], elements[i]['item-description'], elements[i]['item-status']]);
+                console.log(dataSet);
+                console.log(143);
+            }
+            console.log(144);
+
+            $(document).ready(function () {
+                $(tableDivId).DataTable({
+                    data: dataSet,
+                    'columnDefs': [{
+                        'targets': 0,
+                        "width": "20%",
+                        'searchable': false,
+                        'orderable': false,
+                        'className': 'dt-body-center',
+                        'render': function (data, type, full, meta) {
+                            return '<input type="checkbox" name="id[]" value="' + $('<div/>').text(data).html() + '">';
+                        },
+                        order: [[1, 'asc']]
+
+                    }],
+                    columns: [
+                        {title: " ", width: '50px'},
+                        {title: "Item Name", width: '120px'},
+                        {title: "Quantity", width: '100px'},
+                        {title: "Price", width: '100px'},
+                        {title: "Description", width: '200px'},
+                        {title: "Status"},
+                    ]
+                });
+                //$("div.toolbar").html('<b>Custom tool bar! Text/images etc.</b>');
+            });
+        }
+    });
+}
+
+
+$( document ).ready(function() {
+    //get the lists belonging to this group
     getLists().then(ret => {
         console.log(ret);
         console.log(ret['status']);
         if (ret['status'] === 200) {
             console.log("Creating group page lists");
-           // const lists = ret['lists'];
-           // console.log(lists);
-
             const lists = ret['lists'];
             console.log(lists);
 
-            listUuid = lists[0]['list-id'];
-            //print(listUuid);
-
             //populate individual list
-            getElements().then(ret => {
-                console.log(ret);
-                console.log(ret['status']);
-                if (ret['status'] === 200) {
-                    console.log("Creating list display");
-                }
-                const elements = ret['elements'];
-                console.log(elements);
-
-                $("single-list-entry").append("<ul></ul>");
-
-                let dataSet = [];
-
-                console.log(139);
-                for (let i = 0; i < elements.length; i++) {
-                    dataSet.push([elements[i]['item-name'], elements[i]['item-quantity'],  elements[i]['item-cost'], elements[i]['item-description'], elements[i]['item-status']]);
-                    console.log(dataSet);
-                    console.log(143);
-                }
-                console.log(144);
-
-                $(document).ready(function() {
-                    $('#data-table').DataTable( {
-                        data: dataSet,
-                        'columnDefs': [{
-                            'targets': 0,
-                            "width": "20%",
-                            'searchable':false,
-                            'orderable':false,
-                            'className': 'dt-body-center',
-                            'render': function (data, type, full, meta){
-                                return '<input type="checkbox" name="id[]" value="' + $('<div/>').text(data).html() + '">';
-                            }
-
-                        }],
-                        columns: [
-                            { title: "Item Name" },
-                            { title: "Quantity" },
-                            { title: "Price" },
-                            { title: "Description." },
-                            { title: "Status" },
-                        ]
-                    } );
-                } );
-
-            });
+            for(let j = 0; j < lists.length; j++)
+            {
+                listUuid = lists[j]['list-id'];
+                renderTable(getTableDivId(j));
+            }
         }
     });
 
 
 });
+
